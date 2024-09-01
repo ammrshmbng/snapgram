95c4399db62f7c498d63c437e82efd699d6595ac
ammrshmbng <ammarmare01@gmail.com>
2024-09-01 15:13:11 +0700

    putting AuthLayout and RootLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import './globals.css'
-import { Home } from './_root/pages'
-import SigninForm from './_auth/forms/SigninForm'
+import "./globals.css";
+import { Home } from "./_root/pages";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
     <main className="flex h-screen">
-        <Routes>
-          {/* public routes */}
-          <Route path='/sign-in' element={<SigninForm/>} />
-          {/* private routes */}
-          <Route index element={<Home/>} />
-        </Routes>
-    </main>
-  )
-}
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-export default App
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
