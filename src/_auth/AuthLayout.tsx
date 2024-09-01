dc9584b07fbd258dffd349db472c5284955c2cee
ammrshmbng <ammarmare01@gmail.com>
2024-09-01 17:20:07 +0700

    install form and define it

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -3,22 +3,24 @@ import { Outlet, Navigate } from "react-router-dom";
 const AuthLayout = () => {
   const isAuthenticated = false;
   return (
-      <>
-        {
-          isAuthenticated ? 
-          (<Navigate to="/" />)
-          : (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-col items-center justify-center flex-1 py-10">
+            <Outlet />
+          </section>
 
-            <>
-                <section>
-                  <Outlet/>
-                </section>
-            </>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden object-cover w-1/2 h-screen bg-no-repeat xl:block"
+          />
+        </>
+      )}
+    </>
+  );
+};
 
-          )
-        }
-      </>
-  )
-}
-
-export default AuthLayout
+export default AuthLayout;
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,30 @@
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 const SignupForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+
   return (
     <div>
       <Button>Click me</Button>
