177adc6c23f6390a5461b41a053ad342b31c57fb
ammrshmbng <ammarmare01@gmail.com>
2024-09-04 09:26:24 +0700

    simple auth is implemented

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,8 +1,135 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { SigninValidation } from "@/lib/validation";
+import Loader from "@/components/shared/Loader";
+import { Link, useNavigate } from "react-router-dom";
+import { useToast } from "@/hooks/use-toast";
+import { useSignInAccount } from "@/lib/react-query/queries";
+import { useUserContext } from "@/context/AuthContext";
 
 const SigninForm = () => {
-  return (
-    <div>SigninForm</div>
-  )
-}
+  const { toast } = useToast();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
 
-export default SigninForm
+  const { mutateAsync: signInAccount} =
+    useSignInAccount();
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SigninValidation>>({
+    resolver: zodResolver(SigninValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(value: z.infer<typeof SigninValidation>) {
+    
+    const session = await signInAccount({
+      email: value.email,
+      password: value.password,
+    });
+
+    
+
+    if (!session) {
+      toast({ title: "Login failed. Please try again." });
+      return;
+    }
+
+    const isLoggedIn = await checkAuthUser();
+
+    if (isLoggedIn) {
+      form.reset();
+
+
+      navigate("/");
+    } else {
+      toast({ title: "Login failed. Please try again." });
+
+      return;
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <div className="flex-col sm:w-420 flex-center">
+        <img src="/assets/images/logo.svg" alt="logo" />
+        <h2 className="pt-5 h3-bold md:h2-bold sm:pt-12">
+          Log in to your account
+        </h2>
+        <p className="mt-2 text-light-3 small-medium md:base-regular">
+          Welcome back! Please enter your details.
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col w-full gap-5 mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Email</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="shad-form_label">Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="shad-button_primary">
+            {isUserLoading ? (
+              <div className="gap-2 flex-center">
+                <Loader /> Loading...
+              </div>
+            ) : (
+              "Sign in"
+            )}
+          </Button>
+          <p className="mt-2 text-center text-small-regular text-light-2">
+          Don&apos;t have an account?
+            <Link
+              to="/sign-up"
+              className="ml-1 text-primary-500 text-small-semibold"
+            >
+              Sign up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
+};
+
+export default SigninForm;
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -60,7 +60,8 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     const cookieFallback = localStorage.getItem("cookieFallback");
     if (
-      cookieFallback === "[]"
+      cookieFallback === "[]" ||
+      cookieFallback === null 
     ) {
       navigate("/sign-in");
     }
