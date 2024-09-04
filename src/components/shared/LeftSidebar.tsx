c253da9428a2aeda5c71105e98e15cbd01449fdc
ammrshmbng <ammarmare01@gmail.com>
2024-09-04 11:29:35 +0700

    sidebar layout is done

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,8 +1,60 @@
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { Button } from "../ui/button";
 
 const LeftSidebar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>LeftSidebar</div>
-  )
-}
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex items-center gap-3">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
 
-export default LeftSidebar
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route;
+
+            return (
+              <li
+                key={link.label}
+                className={`leftsidebar-link group ${
+                  isActive && "bg-primary-500"
+                }`}
+              >
+                <NavLink
+                  to={link.route}
+                  className="flex items-center gap-4 p-4"
+                >
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${
+                      isActive && "invert-white"
+                    }`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <Button
+        variant="ghost"
+        className="shad-button_ghost"
+        onClick={()=>{}}>
+        <img src="/assets/icons/logout.svg" alt="logout" />
+        <p className="small-medium lg:base-medium">Logout</p>
+      </Button>
+    </nav>
+  );
+};
+
+export default LeftSidebar;
