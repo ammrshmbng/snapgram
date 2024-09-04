a3c3784f8f8a0073d922a49def2ff466f3bd275b
ammrshmbng <ammarmare01@gmail.com>
2024-09-04 13:48:15 +0700

    create post logo

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,7 +1,20 @@
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className="flex flex-1">
+    <div className="common-container">
+      <div className="justify-start w-full max-w-5xl gap-3 flex-start">
+        <img
+          src="/assets/icons/add-post.svg"
+          width={36}
+          height={36}
+          alt="add"
+        />
+        <h2 className="w-full text-left h3-bold md:h2-bold">Create Post</h2>
+      </div>
+
+    </div>
+  </div>
   )
 }
 
