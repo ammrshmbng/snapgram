9e8535810a3d2782c3c7eb6c65e19ef5ab1ea1c6
ammrshmbng <ammarmare01@gmail.com>
2024-09-05 17:36:59 +0700

    back button in postdetail

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,8 +1,35 @@
+import { Button } from "@/components/ui";
+import { useGetPostById } from "@/lib/react-query/queries";
+import { useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
-  return (
-    <div>PostDetails</div>
-  )
-}
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-export default PostDetails
+
+  const { data: post, isPending } = useGetPostById(id);
+
+  return (
+    <div className="post_details-container">
+      <div className="hidden w-full max-w-5xl md:flex">
+        <Button
+          onClick={() => navigate(-1)}
+          variant="ghost"
+          className="shad-button_ghost">
+          <img
+            src={"/assets/icons/back.svg"}
+            alt="back"
+            width={24}
+            height={24}
+          />
+          <p className="small-medium lg:base-medium">Back</p>
+        </Button>
+      </div>
+
+      
+        
+    </div>
+  );
+};
+
+export default PostDetails;
