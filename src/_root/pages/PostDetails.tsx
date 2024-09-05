f9708bd0d555e5c9ed64ab8fcf344ee08333bc5c
ammrshmbng <ammarmare01@gmail.com>
2024-09-05 17:40:00 +0700

    detail post is appeared

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,13 +1,19 @@
+import { Loader } from "@/components/shared";
+import PostStats from "@/components/shared/PostStats";
 import { Button } from "@/components/ui";
+import { useUserContext } from "@/context/AuthContext";
 import { useGetPostById } from "@/lib/react-query/queries";
-import { useNavigate, useParams } from "react-router-dom";
+import { multiFormatDateString } from "@/lib/utils";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const PostDetails = () => {
   const { id } = useParams();
   const navigate = useNavigate();
+  const { user } = useUserContext();
 
 
-  const { data: post, isPending } = useGetPostById(id);
+
+  const { data: post, isPending: isLoading } = useGetPostById(id);
 
   return (
     <div className="post_details-container">
@@ -26,7 +32,94 @@ const PostDetails = () => {
         </Button>
       </div>
 
-      
+      {isLoading || !post ? (
+        <Loader />
+      ) : (
+        <div className="post_details-card">
+          <img
+            src={post?.imageUrl}
+            alt="creator"
+            className="post_details-img"
+          />
+
+          <div className="post_details-info">
+            <div className="w-full flex-between">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3">
+                <img
+                  src={
+                    post?.creator.imageUrl ||
+                    "/assets/icons/profile-placeholder.svg"
+                  }
+                  alt="creator"
+                  className="w-8 h-8 rounded-full lg:w-12 lg:h-12"
+                />
+                <div className="flex flex-col gap-1">
+                  <p className="base-medium lg:body-bold text-light-1">
+                    {post?.creator.name}
+                  </p>
+                  <div className="gap-2 flex-center text-light-3">
+                    <p className="subtle-semibold lg:small-regular ">
+                      {multiFormatDateString(post?.$createdAt)}
+                    </p>
+                    •
+                    <p className="subtle-semibold lg:small-regular">
+                      {post?.location}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+
+              <div className="gap-4 flex-center">
+                <Link
+                  to={`/update-post/${post?.$id}`}
+                  className={`${user.id !== post?.creator.$id && "hidden"}`}>
+                  <img
+                    src={"/assets/icons/edit.svg"}
+                    alt="edit"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
+
+                <Button
+                  onClick={()=>{}}
+                  variant="ghost"
+                  className={`ost_details-delete_btn ${
+                    user.id !== post?.creator.$id && "hidden"
+                  }`}>
+                  <img
+                    src={"/assets/icons/delete.svg"}
+                    alt="delete"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
+              </div>
+            </div>
+
+            <hr className="w-full border border-dark-4/80" />
+
+            <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
+              <p>{post?.caption}</p>
+              <ul className="flex gap-1 mt-2">
+                {post?.tags.map((tag: string, index: string) => (
+                  <li
+                    key={`${tag}${index}`}
+                    className="text-light-3 small-regular">
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="w-full">
+              <PostStats post={post} userId={user.id} />
+            </div>
+          </div>
+        </div>
+      )}
         
     </div>
   );
