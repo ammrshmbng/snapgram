3daa5b5353468f41488458d834774acaa21fef5b
ammrshmbng <ammarmare01@gmail.com>
2024-09-06 21:06:19 +0700

    add some in signupform

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Outlet, Navigate } from "react-router-dom";
+
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (<Navigate to="/sign-in" />)
+      :(
+        <section className="flex flex-col items-center justify-center flex-1 py-10 bdr">
+          <Outlet />
+        </section>
+      )  
+    }
+    </>
   )
 }
 
