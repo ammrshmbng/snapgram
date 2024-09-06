ec041f1504505d59e9af8a8d34752d20ce3d959e
ammrshmbng <ammarmare01@gmail.com>
2024-09-06 21:13:55 +0700

    image auth layout is appear

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -7,9 +7,17 @@ const AuthLayout = () => {
     <>
       {isAuthenticated ? (<Navigate to="/sign-in" />)
       :(
-        <section className="flex flex-col items-center justify-center flex-1 py-10 bdr">
+       <>
+         <section className="flex flex-col items-center justify-center flex-1 py-10 bdr">
           <Outlet />
         </section>
+
+        <img
+            src="./assets/images/side-img.svg"
+            alt="logo"
+            className="hidden object-cover w-1/2 h-screen bg-no-repeat xl:block"
+          />
+       </>
       )  
     }
     </>
