047430ab70566feccbb4aa3d886c1e57d9f6831e
ammrshmbng <ammarmare01@gmail.com>
2024-09-08 19:28:28 +0700

    create config for appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -32,10 +32,11 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values);
+    // const newUser = await createUserAccount(values)
   }
 
   return (
