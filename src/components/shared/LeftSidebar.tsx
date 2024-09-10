88be58a8ba32200288e3a5b083b2eb8a771244be
ammrshmbng <ammarmare01@gmail.com>
2024-09-10 12:38:54 +0700

    leftSidebar is done

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import { useUserContext } from "@/context/AuthContext";
+import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
+import Loader from "./Loader";
+import { Button } from "../ui/button";
+import { useSignOutAccount } from "@/lib/react-query/queries";
+import { useEffect } from "react";
+import { sidebarLinks } from "@/constants";
+import { INavLink } from "@/types";
 
 const LeftSidebar = () => {
-  return (
-    <div>LeftSidebar</div>
-  )
-}
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const { user, setUser, setIsAuthenticated, isLoading } = useUserContext();
 
-export default LeftSidebar
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess]);
+
+  return (
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex items-center gap-3">
+          <img
+            src="/assets/images/logo.svg"
+            alt="logo"
+            width={170}
+            height={36}
+          />
+        </Link>
+
+        {isLoading || !user.email ? (
+          <div className="h-14">
+            <Loader />
+          </div>
+        ) : (
+          <Link to={`/profile/${user.id}`} className="flex items-center gap-3">
+            <img
+              src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+              alt="profile"
+              className="rounded-full h-14 w-14"
+            />
+            <div className="flex flex-col">
+              <p className="body-bold">{user.name}</p>
+              <p className="small-regular text-light-3">@{user.username}</p>
+            </div>
+          </Link>
+        )}
+
+        <ul className="flex flex-col gap-6">
+          {sidebarLinks.map((link: INavLink) => {
+            const isActive = pathname === link.route;
+
+            return (
+              <li
+                key={link.label}
+                className={`leftsidebar-link group ${
+                  isActive && "bg-primary-500"
+                }`}>
+                <NavLink
+                  to={link.route}
+                  className="flex items-center gap-4 p-4">
+                  <img
+                    src={link.imgURL}
+                    alt={link.label}
+                    className={`group-hover:invert-white ${
+                      isActive && "invert-white"
+                    }`}
+                  />
+                  {link.label}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <Button
+        variant="ghost"
+        className="shad-button_ghost"
+        onClick={(e) => signOut()}>
+        <img src="/assets/icons/logout.svg" alt="logout" />
+        <p className="small-medium lg:base-medium">Logout</p>
+      </Button>
+
+    </nav>
+  );
+};
+
+export default LeftSidebar;
