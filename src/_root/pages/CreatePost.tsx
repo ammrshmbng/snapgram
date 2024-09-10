cec955723592e60f8c1c0ec82370255a37f74d75
ammrshmbng <ammarmare01@gmail.com>
2024-09-10 16:42:41 +0700

    some display top for create post

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
-  )
-}
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="justify-start w-full max-w-5xl gap-3 flex-start">
+          <img
+            src="/assets/icons/add-post.svg"
+            width={36}
+            height={36}
+            alt="add"
+          />
+          <h2 className="w-full text-left h3-bold md:h2-bold">Create Post</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
