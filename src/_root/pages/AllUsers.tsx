ef59bfb14ff4f21931bcef72445e02be01265f52
ammrshmbng <ammarmare01@gmail.com>
2024-09-17 15:25:03 +0700

    add allusers and saved featured

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Loader, UserCard } from "@/components/shared";
+import { useToast } from "@/hooks/use-toast";
+import { useGetUsers } from "@/lib/react-query/queries";
 
 const AllUsers = () => {
-  return (
-    <div>AllUsers</div>
-  )
-}
+  const { toast } = useToast();
 
-export default AllUsers
+  const { data: creators, isLoading, isError: isErrorCreators } = useGetUsers();
+
+  if (isErrorCreators) {
+    toast({ title: "Something went wrong." });
+    
+    return;
+  }
+
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="w-full text-left h3-bold md:h2-bold">All Users</h2>
+        {isLoading && !creators ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {creators?.documents.map((creator) => (
+              <li key={creator?.$id} className="flex-1 min-w-[200px] w-full  ">
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
